2410b5c4d1f34786c34bf2ef739022a36e537b77
Alessandro Lambertini <lambertinialessandro16@gmail.com>
2022-03-08 23:37:05 +0100

    added bcolors class and class rename

--- utility.py
+++ utility.py
@@ -24,14 +24,15 @@ class PrintManager:
         plt.imshow(img)
 
     # Print iterations progress
-    def printProgressBar (self, iteration, total, length = 50):
-        percent = ("{0:." + str(2) + "f}").format(100 * (iteration / float(total)))
-        filledLength = int(length * iteration // total)
-        bar = '█' * filledLength + '-' * (length - filledLength)
-        print(f'\r Progress:|{bar}| {percent}% Complete', end='\r')
-        # Print New Line on Complete
-        if iteration == total:
-            print()
+    def printProgressBarI (self, iteration, total, length = 50):
+        if self.FLAG_INFO_PRINT:
+            percent = ("{0:." + str(2) + "f}").format(100 * (iteration / float(total)))
+            filledLength = int(length * iteration // total)
+            bar = '█' * filledLength + '-' * (length - filledLength)
+            print(f'\r### Info Progress:|{bar}| {percent}% Complete', end='\r')
+            # Print New Line on Complete
+            if iteration == total:
+                print()
 
     # Info
     def printI(self, msg):
@@ -42,6 +43,14 @@ class PrintManager:
       if self.FLAG_INFO_PRINT:
         plt.title("### Info "+title)
         plt.imshow(img)
+
+class bcolors:
+    OKBLUE = '\x1b[1;30;44m'
+    OKYELLOW = '\x1b[1;33;40m'
+    OKGREEN = '\x1b[1;32;40m'
+    WARNING = '\x1b[0;30;41m'
+    FAIL = '\x1b[0;30;43m'
+    ENDC = '\x1b[0m'
 
 # Testing
 def main():
