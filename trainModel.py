de7d0b447ac46eb9225a75e7daf7ee8692282e15
Alessandro Lambertini <lambertinialessandro16@gmail.com>
2022-03-23 17:39:01 +0100

    Update trainModel.py

--- trainModel.py
+++ trainModel.py
@@ -5,8 +5,6 @@ Created on Wed Mar  9 14:36:00 2022
 @author: lambe
 """
 
-# from keras_video import SlidingFrameGenerator
-
 import os
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 
@@ -16,8 +14,6 @@ import random
 import numpy as np
 import matplotlib.pyplot as plt
 
-import torch
-
 from loadData import DataLoader
 import buildModel
 
@@ -26,14 +22,16 @@ from utility import PrintManager, bcolors
 
 pm = PrintManager(params.FLAG_DEBUG_PRINT, params.FLAG_INFO_PRINT)
 
-model, criterion, optimizer = buildModel.main()
+model, criterion, optimizer = buildModel.build(typeModel=params.typeModel,
+                                               typeCriterion=params.typeCriterion,
+                                               typeOptimizer=params.typeOptimizer)
 
-#fileName = params.dir_Model+"DeepVO_epoch3.pt"
+#fileName = params.dir_Model+"DeepVO_epoch4.pt"
 #model.load_state_dict(torch.load(fileName))
 #print(print("\x1b[1;31;10mLoaded {}\x1b[0m\n".format(fileName)))
 
-BASE_EPOCH = 4
-NUM_EPOCHS = 2
+BASE_EPOCH = 1 # 1 starting epoch
+NUM_EPOCHS = 10 # 10 how many epoch
 img_out = 0
 
 loss_train = []
@@ -45,13 +43,13 @@ for epoch in range(BASE_EPOCH, BASE_EPOCH+NUM_EPOCHS):
   model.train()
   model.training = True
   app_loss_train = []
-  
+
   train_initT = time.time()
   for sequence in params.trainingSeries:
     print(bcolors.LIGHTGREEN+"Sequence: {}".format(sequence)+bcolors.ENDC)
-    X, y = DataLoader(params.dir_Dataset, sequence=sequence)
+    X, y = DataLoader(params.dir_Dataset, suffixType=params.suffixType, sequence=sequence)
     train_numOfBatch = len(X)
-    
+
     for i in range(train_numOfBatch):
       pm.printProgressBarI(i, train_numOfBatch)
       inputs = X[i]
@@ -59,28 +57,30 @@ for epoch in range(BASE_EPOCH, BASE_EPOCH+NUM_EPOCHS):
 
       model.zero_grad()
       model.reset_hidden_states(sizeHidden=params.BACH_SIZE, zero=True)
-        
+
       outputs = model(inputs)
 
       loss = criterion(outputs, labels)
-      optimizer.zero_grad()
+
       loss.backward()
       optimizer.step()
+
       app_loss_train.append(loss.item())
     del X, y, inputs, labels
     gc.collect()
-    
+
     pm.printProgressBarI(train_numOfBatch, train_numOfBatch)
     print("current loss: {}\n\n".format(sum(app_loss_train)/len(app_loss_train)))
   train_elapsedT = time.time() - train_initT
-  
-  
-  
+
+
+
   print(bcolors.LIGHTYELLOW+"TESTING"+bcolors.ENDC)
   model.eval()
   model.training = False
-  X, y = DataLoader(params.dir_Dataset, sequence=random.choice(params.testingSeries))
-  test_numOfBatch = np.min([5, len(X)])
+  X, y = DataLoader(params.dir_Dataset, suffixType=params.suffixType,
+                    sequence=random.choice(params.testingSeries))
+  test_numOfBatch = np.min([20, len(X)])
   app_loss_test = []
 
   test_initT = time.time()
@@ -89,7 +89,7 @@ for epoch in range(BASE_EPOCH, BASE_EPOCH+NUM_EPOCHS):
     pm.printProgressBarI(i, test_numOfBatch)
     inputs = X[i]
     labels = y[i]
-    
+
     app_outputs = model(inputs)
 
     loss = criterion(app_outputs, labels)
@@ -97,10 +97,10 @@ for epoch in range(BASE_EPOCH, BASE_EPOCH+NUM_EPOCHS):
     outputs.append(app_outputs)
   del X, inputs, labels
   gc.collect()
-  
+
   pm.printProgressBarI(test_numOfBatch, test_numOfBatch)
   test_elapsedT = time.time() - test_initT
-  
+
   y_test_det = []
   for i in range(len(y)):
     y_test_det.append(y[i].detach().numpy())
@@ -117,7 +117,7 @@ for epoch in range(BASE_EPOCH, BASE_EPOCH+NUM_EPOCHS):
 
   print(y_test_det.shape)
   print(outputs_det.shape)
-  
+
   pts_yTest = np.array([[0, 0, 0, 0, 0, 0]])
   pts_out = np.array([[0, 0, 0, 0, 0, 0]])
   for i in range(0, len(outputs_det)):
@@ -130,29 +130,32 @@ for epoch in range(BASE_EPOCH, BASE_EPOCH+NUM_EPOCHS):
   gc.collect()
   print(pts_yTest.shape)
   print(pts_out.shape)
-  
+
   plt.plot(pts_out[:, 0], pts_out[:, 2], color='red')
   plt.plot(pts_yTest[:, 0], pts_yTest[:, 2], color='blue')
+  plt.legend(['out', 'yTest'])
   plt.show()
-  
+
+  ax = plt.axes(projection='3d')
+  ax.plot3D(pts_out[:, 0], pts_out[:, 1], pts_out[:, 2], color='red')
+  ax.plot3D(pts_yTest[:, 0], pts_yTest[:, 1], pts_yTest[:, 2], color='blue')
+  plt.legend(['out', 'yTest'])
+  plt.show()
+  del pts_yTest, pts_out
+  gc.collect()
+
   loss_train.append(sum(app_loss_train)/len(app_loss_train))
   loss_test.append(sum(app_loss_test)/len(app_loss_test))
 
   print("\nepoch %d"%(epoch))
-  print("loss_train %.3f, time %.2fs"%(loss_train[-1], train_elapsedT))
-  print("loss_test %.3f, time %.2fs"%(loss_test[-1], test_elapsedT))
-  
-  
-  
+  print("loss_train %.5f, time %.2fs"%(loss_train[-1], train_elapsedT))
+  print("loss_test %.5f, time %.2fs"%(loss_test[-1], test_elapsedT))
+
+
+
   #Save the model
-  fileName = params.dir_Model+"DeepVO_epoch{}.pt".format(epoch)
-  torch.save(model.state_dict(), fileName)
-  print(print("\x1b[1;31;10mSaved {}\x1b[0m\n".format(fileName)))
-
-
-
-
-
-
+  #fileName = params.dir_Model+"DeepVO_epoch{}.pt".format(epoch)
+  #torch.save(model.state_dict(), fileName)
+  #print(print("\x1b[1;31;10mSaved {}\x1b[0m\n".format(fileName)))
 
 
