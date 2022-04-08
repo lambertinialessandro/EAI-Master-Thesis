cc59deedd571424aac60f1e00859d85f9cdab9ef
Alessandro Lambertini <lambertinialessandro16@gmail.com>
2022-04-08 14:39:02 +0200

    added HTMLProgressBarI

--- utility.py
+++ utility.py
@@ -6,7 +6,9 @@ Created on Thu Feb  3 11:30:41 2022
 """
 
 import matplotlib.pyplot as plt
-from IPython.display import HTML
+from IPython.display import HTML, display
+
+from params import FLAG_DEBUG_PRINT, FLAG_INFO_PRINT
 
 class PrintManager:
     def __init__(self, FLAG_DEBUG_PRINT=False, FLAG_INFO_PRINT=False):
@@ -14,14 +16,15 @@ class PrintManager:
         self.FLAG_INFO_PRINT = FLAG_INFO_PRINT
 
     # Debug
-    def printD(self, msg):
+    def printD(self, msg, head=""):
       if self.FLAG_DEBUG_PRINT:
-        print("### Debug: " + msg)
+        print(head + "### Debug: " + msg)
 
     def imshowD(self, img, title=""):
       if self.FLAG_DEBUG_PRINT:
-        plt.title("### Debug "+title)
+        plt.title("### Debug " + title)
         plt.imshow(img)
+        plt.show()
 
     # Print iterations progress
     def printProgressBarI (self, iteration, total, length = 40):
@@ -34,39 +37,64 @@ class PrintManager:
             if iteration == total:
                 print()
 
+    def HTMLProgressBarI(self, value, max=100):
+        if self.FLAG_INFO_PRINT:
+            return HTML("""
+              <progress value='{value}' max='{max}', style='width: 50%' >
+                  {value}
+              </progress>
+            """.format(value=value, max=max))
+
     # Info
-    def printI(self, msg):
+    def printI(self, msg, head=""):
       if self.FLAG_INFO_PRINT:
-        print("### Info: " + msg)
+        print(head + "### Info: " + msg)
 
     def imshowI(self, img, title=""):
       if self.FLAG_INFO_PRINT:
-        plt.title("### Info "+title)
+        plt.title("### Info " + title)
         plt.imshow(img)
+        plt.show()
 
 class bcolors:
     LIGHTRED = '\x1b[1;31;10m'
     LIGHTGREEN = '\x1b[1;32;10m'
     LIGHTYELLOW = '\x1b[1;33;10m'
-    
+
     DARKBLUE = '\x1b[1;30;44m'
     DARKYELLOW = '\x1b[1;33;40m'
     DARKGREEN = '\x1b[1;32;40m'
-    
+
     WARNING = '\x1b[0;30;41m'
     FAIL = '\x1b[0;30;43m'
-    
+
     ENDC = '\x1b[0m'
+
+PM = PrintManager(FLAG_DEBUG_PRINT, FLAG_INFO_PRINT)
+
 
 # Testing
 def main():
+    import time
+
     FLAG_DEBUG_PRINT = True
     FLAG_INFO_PRINT = True
 
-    mm = PrintManager(FLAG_DEBUG_PRINT, FLAG_INFO_PRINT)
+    pm = PrintManager(FLAG_DEBUG_PRINT, FLAG_INFO_PRINT)
 
-    mm.printD("ciao")
-    mm.printI("ciao")
+    pm.printD(bcolors.LIGHTGREEN+"ciao"+bcolors.ENDC)
+    pm.printI(bcolors.DARKGREEN+"ciao"+bcolors.ENDC)
+
+    for i in range(5):
+        pm.printProgressBarI(i, 5)
+        time.sleep(0.3)
+    pm.printProgressBarI(5, 5)
+
+    # working on colab
+    progress = display(pm.HTMLProgressBarI(0, 5-1), display_id=True)
+    for i in range(5):
+        progress.update(pm.HTMLProgressBarI(i, 5-1))
+        time.sleep(0.2)
 
 if __name__ == "__main__":
     main()
