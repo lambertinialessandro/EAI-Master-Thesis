09442a491b8a2823b9d9f10db4a1682ed5564bf8
Alessandro Lambertini <lambertinialessandro16@gmail.com>
2022-04-09 08:50:36 +0200

    LSTM intead of LSTMCell

--- QuaternionDeepVONet.py
+++ QuaternionDeepVONet.py
@@ -51,33 +51,42 @@ class QuaternionDeepVONet(nn.Module):
         self.block6 = QuatC_Block (512, 1024, kernel_size=(3, 3), stride=(2, 2),
                                    padding=(1, 1), dropout_rate=0.2)
 
-        self.dimLSTMCell1 = DIM_LSTM
-        self.lstm1 = nn.LSTMCell(self.dimLSTMCell1, 1000)
-        self.lstm1_dropout = nn.Dropout(0.5)
-        self.lstm2 = nn.LSTMCell(1000, 1000)
-        self.lstm2_dropout = nn.Dropout(0.5)
+        # self.dimLSTMCell1 = DIM_LSTM
+        # self.lstm1 = nn.LSTMCell(self.dimLSTMCell1, 1000)
+        # self.lstm1_dropout = nn.Dropout(0.5)
+        # self.lstm2 = nn.LSTMCell(1000, 1000)
+        # self.lstm2_dropout = nn.Dropout(0.5)
 
-        self.fc = nn.Linear(in_features=1000, out_features=6)
+        # self.fc = nn.Linear(in_features=1000, out_features=6)
 
-        self.reset_hidden_states(sizeHidden=sizeHidden, zero=True)
+        # self.reset_hidden_states(sizeHidden=sizeHidden, zero=True)
 
-    def reset_hidden_states(self, sizeHidden=1, zero=True):
-        if zero == True:
-            self.hx1 = Variable(torch.zeros(sizeHidden, 1000))
-            self.cx1 = Variable(torch.zeros(sizeHidden, 1000))
-            self.hx2 = Variable(torch.zeros(sizeHidden, 1000))
-            self.cx2 = Variable(torch.zeros(sizeHidden, 1000))
-        else:
-            self.hx1 = Variable(self.hx1.data)
-            self.cx1 = Variable(self.cx1.data)
-            self.hx2 = Variable(self.hx2.data)
-            self.cx2 = Variable(self.cx2.data)
+        self.flatten = nn.Flatten()
 
-        if next(self.parameters()).is_cuda == True:
-            self.hx1 = self.hx1.cuda()
-            self.cx1 = self.cx1.cuda()
-            self.hx2 = self.hx2.cuda()
-            self.cx2 = self.cx2.cuda()
+        self.dimLSTM = DIM_LSTM
+        self.lstm = nn.LSTM(input_size=self.dimLSTM, hidden_size=1000, num_layers=2,
+                            dropout=0.5, batch_first=True)
+        self.lstm_dropout = nn.Dropout(0.5)
+
+        self.linear_output = nn.Linear(in_features=1000, out_features=6)
+
+    # def reset_hidden_states(self, sizeHidden=1, zero=True):
+    #     if zero == True:
+    #         self.hx1 = Variable(torch.zeros(sizeHidden, 1000))
+    #         self.cx1 = Variable(torch.zeros(sizeHidden, 1000))
+    #         self.hx2 = Variable(torch.zeros(sizeHidden, 1000))
+    #         self.cx2 = Variable(torch.zeros(sizeHidden, 1000))
+    #     else:
+    #         self.hx1 = Variable(self.hx1.data)
+    #         self.cx1 = Variable(self.cx1.data)
+    #         self.hx2 = Variable(self.hx2.data)
+    #         self.cx2 = Variable(self.cx2.data)
+
+    #     if next(self.parameters()).is_cuda == True:
+    #         self.hx1 = self.hx1.cuda()
+    #         self.cx1 = self.cx1.cuda()
+    #         self.hx2 = self.hx2.cuda()
+    #         self.cx2 = self.cx2.cuda()
 
     def forward(self, x):
         x = self.block1(x)
@@ -90,16 +99,23 @@ class QuaternionDeepVONet(nn.Module):
         x = self.block5_1(x)
         x = self.block6(x)
 
-        #print(x.size())
-        x = x.view(x.size(0), self.dimLSTMCell1)
-        #print(x.size())
-        self.hx1, self.cx1 = self.lstm1(x, (self.hx1, self.cx1))
-        x = self.lstm1_dropout(self.hx1)
+        # #print(x.size())
+        # x = x.view(x.size(0), self.dimLSTMCell1)
+        # #print(x.size())
+        # self.hx1, self.cx1 = self.lstm1(x, (self.hx1, self.cx1))
+        # x = self.lstm1_dropout(self.hx1)
 
-        self.hx2, self.cx2 = self.lstm2(x, (self.hx2, self.cx2))
-        x = self.lstm2_dropout(self.hx2)
+        # self.hx2, self.cx2 = self.lstm2(x, (self.hx2, self.cx2))
+        # x = self.lstm2_dropout(self.hx2)
 
-        x = self.fc(x)
+        # x = self.fc(x)
+
+        x = self.flatten(x)
+
+        x, _ = self.lstm(x)
+        x = self.lstm_dropout(x)
+
+        x = self.linear_output(x)
         return x
 
 
