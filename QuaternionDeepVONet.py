7ded2de1b8011686d1354ae809a92056f07bb72a
Alessandro Lambertini <lambertinialessandro16@gmail.com>
2022-05-11 12:33:34 +0200

    added QuaternionDeepVONet_FSM

--- QuaternionDeepVONet.py
+++ QuaternionDeepVONet.py
@@ -7,16 +7,15 @@ Created on Tue Mar 22 15:54:35 2022
 
 import torch
 import torch.nn as nn
-from torch.autograd import Variable
 
-import quaternionFunctions as QF
-from params import DIM_LSTM
+from quaternionFunctions import QuaternionConv
+from params import DIM_LSTM, HIDDEN_SIZE_LSTM
 
 class QuatC_Block(nn.Module):
   def __init__(self, in_ch, out_ch, kernel_size, stride, padding, dropout_rate):
     super(QuatC_Block, self).__init__()
 
-    self.conv = QF.QuaternionConv(in_ch, out_ch, kernel_size=kernel_size, stride=stride, padding=padding)
+    self.conv = QuaternionConv(in_ch, out_ch, kernel_size=kernel_size, stride=stride, padding=padding)
     self.relu = nn.ReLU(inplace=True)
     self.batch = nn.BatchNorm2d(out_ch)
     self.drop = nn.Dropout(dropout_rate)
@@ -29,7 +28,7 @@ class QuatC_Block(nn.Module):
     return x
 
 class QuaternionDeepVONet(nn.Module):
-    def __init__(self, sizeHidden=1):
+    def __init__(self):
         super(QuaternionDeepVONet, self).__init__()
 
         self.block1 = QuatC_Block(8, 64, kernel_size=(7, 7), stride=(2, 2),
@@ -51,42 +50,15 @@ class QuaternionDeepVONet(nn.Module):
         self.block6 = QuatC_Block (512, 1024, kernel_size=(3, 3), stride=(2, 2),
                                    padding=(1, 1), dropout_rate=0.2)
 
-        # self.dimLSTMCell1 = DIM_LSTM
-        # self.lstm1 = nn.LSTMCell(self.dimLSTMCell1, 1000)
-        # self.lstm1_dropout = nn.Dropout(0.5)
-        # self.lstm2 = nn.LSTMCell(1000, 1000)
-        # self.lstm2_dropout = nn.Dropout(0.5)
-
-        # self.fc = nn.Linear(in_features=1000, out_features=6)
-
-        # self.reset_hidden_states(sizeHidden=sizeHidden, zero=True)
-
         self.flatten = nn.Flatten()
 
         self.dimLSTM = DIM_LSTM
-        self.lstm = nn.LSTM(input_size=self.dimLSTM, hidden_size=1000, num_layers=2,
+        self.hidden_size = HIDDEN_SIZE_LSTM
+        self.lstm = nn.LSTM(input_size=self.dimLSTM, hidden_size=self.hidden_size, num_layers=2,
                             dropout=0.5, batch_first=True)
         self.lstm_dropout = nn.Dropout(0.5)
 
         self.linear_output = nn.Linear(in_features=1000, out_features=6)
-
-    # def reset_hidden_states(self, sizeHidden=1, zero=True):
-    #     if zero == True:
-    #         self.hx1 = Variable(torch.zeros(sizeHidden, 1000))
-    #         self.cx1 = Variable(torch.zeros(sizeHidden, 1000))
-    #         self.hx2 = Variable(torch.zeros(sizeHidden, 1000))
-    #         self.cx2 = Variable(torch.zeros(sizeHidden, 1000))
-    #     else:
-    #         self.hx1 = Variable(self.hx1.data)
-    #         self.cx1 = Variable(self.cx1.data)
-    #         self.hx2 = Variable(self.hx2.data)
-    #         self.cx2 = Variable(self.cx2.data)
-
-    #     if next(self.parameters()).is_cuda == True:
-    #         self.hx1 = self.hx1.cuda()
-    #         self.cx1 = self.cx1.cuda()
-    #         self.hx2 = self.hx2.cuda()
-    #         self.cx2 = self.cx2.cuda()
 
     def forward(self, x):
         x = self.block1(x)
@@ -99,17 +71,6 @@ class QuaternionDeepVONet(nn.Module):
         x = self.block5_1(x)
         x = self.block6(x)
 
-        # #print(x.size())
-        # x = x.view(x.size(0), self.dimLSTMCell1)
-        # #print(x.size())
-        # self.hx1, self.cx1 = self.lstm1(x, (self.hx1, self.cx1))
-        # x = self.lstm1_dropout(self.hx1)
-
-        # self.hx2, self.cx2 = self.lstm2(x, (self.hx2, self.cx2))
-        # x = self.lstm2_dropout(self.hx2)
-
-        # x = self.fc(x)
-
         x = self.flatten(x)
 
         x, _ = self.lstm(x)
@@ -118,4 +79,163 @@ class QuaternionDeepVONet(nn.Module):
         x = self.linear_output(x)
         return x
 
+
+class LambdaLayer(nn.Module):
+  def __init__(self, lambd):
+    super(LambdaLayer, self).__init__()
+    self.lambd = lambd
+  def forward(self, x):
+    return self.lambd(x)
+
+class FSM(nn.Module):
+  def __init__(self, debug=False):
+    super(FSM, self).__init__()
+    self.debug = debug
+
+    self.act = nn.ReLU()
+
+    # [64, 512, 3, 3], expected input[1, 256, 24, 16]
+    # [32, 256, 3, 3], expected input[10, 1024, 5, 2]
+
+    C = 1024 # 256
+    Co = int(C // 8)
+
+    self.b1_1 = torch.nn.Conv2d(C, Co, kernel_size=3, stride=1, padding=1)
+    self.b1_2 = torch.nn.BatchNorm2d(Co)
+
+    self.batch, self.dim1, self.dim2, channels = 10, 5, 2, 128 # 24, 16, 32 # 14, 12, 128
+    Ch = (channels // 2)
+    if Ch < 1:
+      Ch = 1
+    self.Ch = Ch
+
+    self.b2 = torch.nn.Conv2d(Co, Ch, kernel_size=1, stride=1, padding=0)
+    self.b3 = torch.nn.Conv2d(Co, Ch, kernel_size=1, stride=1, padding=0)
+    self.b4 = torch.nn.Conv2d(Co, Ch, kernel_size=1, stride=1, padding=0)
+
+    self.b6 = torch.nn.ConvTranspose2d(Ch, channels, kernel_size=1, stride=1, padding=0)
+
+    self.b7_1 = torch.nn.ConvTranspose2d(channels, C, kernel_size=3, stride=1, padding=1)
+    self.b7_2 = torch.nn.BatchNorm2d(C)
+
+
+  def forward(self, x):
+    if self.debug:
+      print("x.shape : ", x.shape)
+
+    b1_1 = self.act(self.b1_1(x))
+    if self.debug:
+      print("b1_1.shape : ", b1_1.shape)
+    b1_2 = self.b1_2(b1_1)
+    if self.debug:
+      print("b1_2.shape : ", b1_2.shape)
+
+    b2 = self.act(self.b2(b1_2))
+    if self.debug:
+      print("b2.shape : ", b2.shape)
+    b2 = torch.reshape(b2, (self.Ch, -1, 1, 1))
+    if self.debug:
+      print("b2.shape : ", b2.shape)
+    b3 = self.act(self.b3(b1_2))
+    if self.debug:
+      print("b3.shape : ", b3.shape)
+    b3 = torch.reshape(b3, (self.Ch, -1, 1, 1))
+    if self.debug:
+      print("b3.shape : ", b3.shape)
+    b4 = self.act(self.b4(b1_2))
+    if self.debug:
+      print("b4.shape : ", b4.shape)
+    b4 = torch.reshape(b4, (self.Ch, -1, 1, 1))
+    if self.debug:
+      print("b4.shape : ", b4.shape)
+
+    b5 = b2@b3 # dot
+    if self.debug:
+      print("b5.shape : ", b5.shape)
+    b5_size = b5.shape
+    Lambda = LambdaLayer(lambda z: (1. / float(b5_size[-1])) * z)
+    b5 = Lambda(b5)
+    if self.debug:
+      print("b5.shape : ", b5.shape)
+
+    b6 = b5@b4 # dot
+    if self.debug:
+      print("b6.shape : ", b6.shape)
+    b6 = torch.reshape(b6, (self.batch, self.Ch, self.dim1, self.dim2))
+    if self.debug:
+      print("b6.shape : ", b6.shape)
+    b6 = self.act(self.b6(b6))
+    if self.debug:
+      print("b6.shape : ", b6.shape)
+    b6 = torch.add(b1_2, b6)
+    if self.debug:
+      print("b6.shape : ", b6.shape)
+
+    b7_1 = self.act(self.b7_1(b6))
+    if self.debug:
+      print("b7_1.shape : ", b7_1.shape)
+    b7_2 = self.b7_2(b7_1)
+    if self.debug:
+      print("b7_2.shape : ", b7_2.shape)
+    out = torch.add(b7_2, x)
+    if self.debug:
+      print("out.shape : ", out.shape)
+
+    return out
+
+class QuaternionDeepVONet_FSM(nn.Module):
+    def __init__(self):
+        super(QuaternionDeepVONet_FSM, self).__init__()
+
+        self.block1 = QuatC_Block(6, 64, kernel_size=(7, 7), stride=(2, 2),
+                              padding=(3, 3), dropout_rate=0.2)
+        self.block2 = QuatC_Block (64, 128, kernel_size=(5, 5), stride=(2, 2),
+                               padding=(2, 2), dropout_rate=0.2)
+        self.block3 = QuatC_Block (128, 256, kernel_size=(5, 5), stride=(2, 2),
+                               padding=(2, 2), dropout_rate=0.2)
+        self.block3_1 = QuatC_Block (256, 256, kernel_size=(3, 3), stride=(1, 1),
+                                 padding=(1, 1), dropout_rate=0.2)
+        self.block4 = QuatC_Block (256, 512, kernel_size=(3, 3), stride=(2, 2),
+                               padding=(1, 1), dropout_rate=0.2)
+        self.block4_1 = QuatC_Block (512, 512, kernel_size=(3, 3), stride=(1, 1),
+                                 padding=(1, 1), dropout_rate=0.2)
+        self.block5 = QuatC_Block (512, 512, kernel_size=(3, 3), stride=(2, 2),
+                               padding=(1, 1), dropout_rate=0.2)
+        self.block5_1 = QuatC_Block (512, 512, kernel_size=(3, 3), stride=(1, 1),
+                                 padding=(1, 1), dropout_rate=0.2)
+        self.block6 = QuatC_Block (512, 1024, kernel_size=(3, 3), stride=(2, 2),
+                               padding=(1, 1), dropout_rate=0.2)
+
+        self.fsm_block = FSM()
+        self.flatten = nn.Flatten()
+
+        self.dimLSTM = DIM_LSTM
+        self.hidden_size = HIDDEN_SIZE_LSTM
+        self.lstm = nn.LSTM(input_size=self.dimLSTM, hidden_size=self.hidden_size, num_layers=2,
+                            dropout=0.5, batch_first=True)
+        self.lstm_dropout = nn.Dropout(0.5)
+
+        self.linear_output = nn.Linear(in_features=1000, out_features=6)
+
+    def forward(self, x):
+        x = self.block1(x)
+        x = self.block2(x)
+        x = self.block3(x)
+        x = self.block3_1(x)
+        x = self.block4(x)
+        x = self.block4_1(x)
+        x = self.block5(x)
+        x = self.block5_1(x)
+        x = self.block6(x)
+
+        x = self.fsm_block(x)
+        x = self.flatten(x)
+        print(x.size())
+
+        x, _ = self.lstm(x)
+        print(x.size())
+        x = self.lstm_dropout(x)
+
+        x = self.linear_output(x)
+        return x
 
