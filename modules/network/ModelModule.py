63e7757f838fe4a6da40343cd5303446e0e5e20b
Alessandro Lambertini <lambertinialessandro16@gmail.com>
2022-06-19 10:32:02 +0200

    added QuaternionSmallDeepVONet

--- modules/network/ModelModule.py
+++ modules/network/ModelModule.py
@@ -1,19 +1,23 @@
 
 from enum import Enum
 
-from modules.network.DeepVONet import DeepVONet, DeepVONet_FSM
-from modules.network.SmallDeepVONet import SmallDeepVONet
-from modules.network.QuaternionDeepVONet import QuaternionDeepVONet, QuaternionDeepVONet_FSM
+import modules.network.model as mnm
+from mnm.DeepVONet import DeepVONet, DeepVONet_FSM
+from mnm.SmallDeepVONet import SmallDeepVONet
+from mnm.QuaternionSmallDeepVONet import QuaternionSmallDeepVONet
+from mnm.QuaternionDeepVONet import QuaternionDeepVONet, QuaternionDeepVONet_FSM
 
 
 class ModelEnum(Enum):
     DeepVONet = "DeepVONet"
-    DeepVONet_FSM = "DeepVONet_FSM"
+    QuaternionDeepVONet = "QuaternionDeepVONet"
 
     SmallDeepVONet = "SmallDeepVONet"
+    QuaternionSmallDeepVONet = "QuaternionSmallDeepVONet"
 
-    QuaternionDeepVONet = "QuaternionDeepVONet"
+    DeepVONet_FSM = "DeepVONet_FSM"
     QuaternionDeepVONet_FSM = "QuaternionDeepVONet_FSM"
+
 
 class ModelFactory():
     def __init__(self):
@@ -23,14 +27,16 @@ class ModelFactory():
     def build(typeModel: ModelEnum, input_size_LSTM, hidden_size_LSTM, device):
         if typeModel == ModelEnum.DeepVONet:
             model = DeepVONet(input_size_LSTM, hidden_size_LSTM).to(device)
-        elif typeModel == ModelEnum.DeepVONet_FSM:
-            model = DeepVONet_FSM(input_size_LSTM, hidden_size_LSTM).to(device)
+        elif typeModel == ModelEnum.QuaternionDeepVONet:
+            model = QuaternionDeepVONet(input_size_LSTM, hidden_size_LSTM).to(device)
 
         elif typeModel == ModelEnum.SmallDeepVONet:
             model = SmallDeepVONet(input_size_LSTM, hidden_size_LSTM).to(device)
+        elif typeModel == ModelEnum.QuaternionSmallDeepVONet:
+            model = QuaternionSmallDeepVONet(input_size_LSTM, hidden_size_LSTM).to(device)
 
-        elif typeModel == ModelEnum.QuaternionDeepVONet:
-            model = QuaternionDeepVONet(input_size_LSTM, hidden_size_LSTM).to(device)
+        elif typeModel == ModelEnum.DeepVONet_FSM:
+            model = DeepVONet_FSM(input_size_LSTM, hidden_size_LSTM).to(device)
         elif typeModel == ModelEnum.QuaternionDeepVONet_FSM:
             model = QuaternionDeepVONet_FSM().to(device)
         else:
