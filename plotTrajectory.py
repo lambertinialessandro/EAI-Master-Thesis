269fd231dd9e41e2dac4dd7353f26c3e13ec8c0d
Alessandro Lambertini <lambertinialessandro16@gmail.com>
2022-08-31 23:18:44 +0200

    Update plotTrajectory.py

--- plotTrajectory.py
+++ plotTrajectory.py
@@ -9,30 +9,18 @@ from modules.utility import PM, bcolors, poseFile2poseRobot
 from modules.preprocess.PreprocessModule import PreprocessFactory
 from loadData import DataGeneretorPreprocessed, DataGeneretorOnline, RandomDataGeneretor
 
-for sequence in params.trainingSeries:
+for sequence in ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10"]:
     path2pose = f"./Dataset/poses/{sequence}.txt"
 
     pts_yTest = np.array([[0, 0, 0, 0, 0, 0]])
 
     posesSet = []
     with open(path2pose, 'r') as f:
-        posef = np.fromstring(f.readline(), dtype=float, sep=' ')
-        pose1 = poseFile2poseRobot(posef)
         for line in f:
-            pose2 = pose1
             posef = np.fromstring(line, dtype=float, sep=' ')
-            pose1 = poseFile2poseRobot(posef)
-
-            pose = pose2-pose1
-
-            pts_yTest = np.append(pts_yTest, [pts_yTest[-1] + pose], axis=0)
+            pts_yTest = np.append(pts_yTest, [poseFile2poseRobot(posef)], axis=0)
 
 
     plt.plot(pts_yTest[:, 0], pts_yTest[:, 2], color='blue')
     plt.legend(['out', 'yTest'])
     plt.show()
-
-    # ax = plt.axes(projection='3d')
-    # ax.plot3D(pts_yTest[:, 0], pts_yTest[:, 1], pts_yTest[:, 2], color='blue')
-    # plt.legend(['out', 'yTest'])
-    # plt.show()
