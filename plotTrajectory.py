23f54073f1a89f2788ae62386493fcbc5e8ea9e3
Alessandro Lambertini <lambertinialessandro16@gmail.com>
2022-09-12 15:59:31 +0200

    Update plotTrajectory.py

--- plotTrajectory.py
+++ plotTrajectory.py
@@ -1,13 +1,9 @@
 
-import os
-import re
+
 import numpy as np
 import matplotlib.pyplot as plt
 
-import params
-from modules.utility import PM, bcolors, poseFile2poseRobot
-from modules.preprocess.PreprocessModule import PreprocessFactory
-from loadData import DataGeneretorPreprocessed, DataGeneretorOnline, RandomDataGeneretor
+from modules.utility import poseFile2poseRobot
 
 for sequence in ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10"]:
     path2pose = f"./Dataset/poses/{sequence}.txt"
